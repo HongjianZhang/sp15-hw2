4d698a424c3beaf9a5f57a7e7f6a915026ac71bc
Luke <zhanghongjian.1995@gmail.com>
2015-02-26 16:26:19 -0800

    sp15-hw2, routing and ruby syntax

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,2 +1,12 @@
 class Foobar
+	def self.baz(lst)
+		result = []
+		for x in lst
+			result.push(x.to_i + 2)
+		end
+		result = result.select {|x| x%2==0}
+		result = result.uniq
+		result = result.delete_if{|num| num > 10}
+		result.inject(:+)
+	end
 end
